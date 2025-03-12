57c970faa12fc189f876cae05bc646a2e16e8c7d
dtri17 <trindde170428@fpt.edu.vn>
2025-03-12 10:31:36 +0700

    update pay

--- controllers/payController.js
+++ controllers/payController.js
@@ -15,7 +15,7 @@ exports.cancel = async (req, res) => {
 
 exports.pay = async (req, res) => {
   try {
-    const userData = req.cookies.userData ? JSON.parse(req.cookies.userData) : null;
+    const userData = req.user
     if (!userData) {
       return res.status(401).render('errors', { message: 'User not authenticated' });
     }
